e97af1f29a4345c647a5467ab8ecfec9f8042bb8
Tsuki-0 <feliperafaelsouza2009@gmail.com>
2026-09-25 10:07:18 -0300

    chore: organizado

--- inc/header.php
+++ inc/header.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>CRUD com Bootstrap</title>
+    <title>Gestão de enfermeiros</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="keywords" content="">
